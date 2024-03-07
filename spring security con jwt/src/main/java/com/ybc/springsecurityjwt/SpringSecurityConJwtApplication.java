af39585ddc5aa305eb2fc80637d7e064364052a3
MatiasRuizHolgado <matias.ruizholgado.external@enel.com>
2024-03-07 22:00:04 +0100

    Initial commit

---
+++ spring security con jwt/src/main/java/com/ybc/springsecurityjwt/SpringSecurityConJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ybc.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityConJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityConJwtApplication.class, args);
+    }
+
+}
---
+++ spring security con jwt/src/test/java/com/ybc/springsecurityjwt/SpringSecurityConJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybc.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
